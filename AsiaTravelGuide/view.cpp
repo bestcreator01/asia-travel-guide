1f65933c088f9a6d9fced62fdfa874f2e8d97ee8
Seoin <u1324614@gcloud.utah.edu>
2023-12-04 18:53:40 -0700

    quiz button to show

--- AsiaTravelGuide/view.cpp
+++ AsiaTravelGuide/view.cpp
@@ -66,7 +66,7 @@ view::view(Model& model, QWidget *parent)
     ui->quizButton->setStyleSheet("QPushButton { background-color: transparent; border: none; }");
     ui->quizButton->setIconSize(QSize(70,70));
     ui->quizButton->setIcon(quizIcon);
-    ui->quizButton->hide();
+    ui->quizButton->show();
     // TODO
 
     // checkmarks
