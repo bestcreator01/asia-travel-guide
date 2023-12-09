d759480d1e9c97109905e221d4e52917c529cd31
Jiwon Park <qkrwldnjs3239@gmail.com>
2023-12-08 21:05:31 -0700

    added comments to model.h

--- AsiaTravelGuide/model.h
+++ AsiaTravelGuide/model.h
@@ -12,7 +12,6 @@
 class Model : public QObject
 {
     Q_OBJECT
-    //enum State{Welcome, AsiaMap, CountryMap};
     QString question1, question2, question3, question4;
     QString option1, option2, option3, option4;
     QList<int> numbers = {0, 1, 2, 3};
@@ -23,17 +22,20 @@ public:
     Model(QObject *parent = nullptr);
 
 public slots:
+
+    ///
+    /// \brief changeState update the previous state based on the current state
+    /// \param state current state
+    ///
     void changeState(QString state);
     void showQuizInfo(QString Country);
     void randomizeOption();
-//    void receiveSignalToSendTextIndia(QString name);
 
 
 signals:
     void changedScreenState(QString state);
     void sendQuiz(QMap<QString, QList<QString>> &questionbank, QList<QString> &questions);
     void sendOptions(QList<int> &numbers);
-//    void sendDescriptions(QString description);
     void sendCountryForBgImage(QString country);
 
 private:
@@ -42,14 +44,6 @@ private:
     void setKorea();
     void putQuestionInQuestionBank(QString question);
     void randomizeQuestions();
-//    QString landMarkInfo[2];
-//    QString restaurantInfo[2];
-
-//    enum Country{India, Korea, Thailand};
-//    enum Place{Restaurant, Landmark};
-
-//    Country currentCountry;
-//    Place currentPlace;
 };
 
 #endif // MODEL_H
